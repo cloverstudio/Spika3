c05008950b6a70507028c92fc9bdadac90d5e381
Max Clover <>
2021-10-15 11:32:19 +0200

    wellcome screen CSS fix

--- client/apps/messenger/src/pages/mocks/login.tsx
+++ client/apps/messenger/src/pages/mocks/login.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Grid, Box, Stack, Paper, Container,TextField, Button, Link } from "@mui/material";
-import image from '../../../../../../documents/pages/login_robot_image.svg';
-import logo from '../../../../../../documents/pages/login_logo.svg';
+import { Grid, Box, Stack, Paper, Container, TextField, Button, Link } from "@mui/material";
+import image from "../../../../../../documents/pages/login_robot_image.svg";
+import logo from "../../../../../../documents/pages/login_logo.svg";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { height, textAlign } from "@mui/system";
 
@@ -10,29 +10,40 @@ const theme = createTheme({
   palette: {
     mode: "light",
   },
-})
+});
 
 export default function () {
   return (
     <ThemeProvider theme={theme}>
-      <Grid container>
-        <Grid  item xs={12} md={6} lg={4}>
-          <Stack justifyContent="center" alignItems="center" spacing={2} sx={{height: '100%'}}>
-            <Box sx={{width: '80%',height: 200,display: { xs: 'block', md: 'none' } }}></Box>
-            <Stack justifyContent="center" alignItems="center" spacing={2} direction="row">
-              <img src={logo}/>
+      <Grid container id="login">
+        <Grid item xs={12} md={6} lg={4} className="welcome_padding">
+          <Stack justifyContent="start" alignItems="start" spacing={2} sx={{ height: "100%" }}>
+            <Stack justifyContent="start" alignItems="start" spacing={2} direction="row">
+              <img src={logo} />
               <label> Spika </label>
             </Stack>
-            <Box sx={{width: '80%',height: 5 }}></Box> 
-            <Box> Welcome! </Box>
-            <Box> Sign in to start using Spika! </Box>
-            <TextField sx={{width: '80%', height:50}} id="outlined-basic" label="email or phone number" variant="outlined" />
-            <Button sx={{width: '80%', height: 56}} variant="outlined">Next</Button>
-            <Box><Link href="#" underline="none">{'New here? Create account'}</Link></Box>
+            <Box className="welcome_title"> Welcome! </Box>
+            <Box className="welcome_sub_title"> Sign in to start using Spika! </Box>
+            <TextField
+              sx={{ width: "80%", height: 50 }}
+              id="outlined-basic"
+              label="email or phone number"
+              variant="outlined"
+            />
+            <Button sx={{ width: "80%", height: 56 }} variant="outlined">
+              Next
+            </Button>
+            <Box className="welcome_create">
+              <Link href="#" underline="none">
+                {"New here? Create account"}
+              </Link>
+            </Box>
           </Stack>
         </Grid>
-        <Grid item md={6} lg={8} >
-          <Box  sx={{ display: { xs: 'none', md: 'block' }, textAlign: 'right'}}><img className="max-height" src={image}/></Box>
+        <Grid item md={6} lg={8}>
+          <Box sx={{ display: { xs: "none", md: "block" }, textAlign: "right" }}>
+            <img className="max-height" src={image} />
+          </Box>
         </Grid>
       </Grid>
     </ThemeProvider>
