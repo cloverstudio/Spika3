36ee205cb6894bba57d8759f9628ed331232cc13
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2021-12-02 21:37:43 +0100

    feat: get and update user

---
+++ server/services/messenger/route/me.ts
@@ -0,0 +1,100 @@
+import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+import { error as le } from "../../../components/logger";
+import validate from "../../../components/validateMiddleware";
+import * as yup from "yup";
+import { successResponse, errorResponse } from "../../../components/response";
+import auth from "../lib/auth";
+import { UserRequest } from "../lib/types";
+import sanitize from "../../../components/sanitize";
+
+const prisma = new PrismaClient();
+
+const updateSchema = yup.object().shape({
+    body: yup.object().shape({
+        telephoneNumber: yup.string().strict(),
+        telephoneNumberHashed: yup.string().strict(),
+        emailAddress: yup.string().strict(),
+        displayName: yup.string().strict(),
+        avatarUrl: yup.string().strict(),
+    }),
+});
+
+export default (): Router => {
+    const router = Router();
+
+    router.get("/", auth, async (req: Request, res: Response) => {
+        const userReq: UserRequest = req as UserRequest;
+
+        try {
+            res.send(successResponse({ user: sanitize("user", userReq.user) }));
+        } catch (e: any) {
+            le(e);
+            res.status(500).send(errorResponse(`Server error ${e}`, userReq.lang));
+        }
+    });
+
+    router.put("/", auth, validate(updateSchema), async (req: Request, res: Response) => {
+        const userReq: UserRequest = req as UserRequest;
+        const id = userReq.user.id;
+
+        try {
+            const { telephoneNumber, telephoneNumberHashed, emailAddress, displayName, avatarUrl } =
+                req.body;
+
+            const userWithSameEmailAddress =
+                emailAddress &&
+                (await prisma.user.findFirst({
+                    where: { emailAddress, id: { not: id } },
+                }));
+            if (userWithSameEmailAddress) {
+                return res
+                    .status(400)
+                    .send(errorResponse("User with that email exists", userReq.lang));
+            }
+
+            const userWithSameTelephoneNumber =
+                telephoneNumber &&
+                (await prisma.user.findFirst({
+                    where: { telephoneNumber, id: { not: id } },
+                }));
+            if (userWithSameTelephoneNumber) {
+                return res
+                    .status(400)
+                    .send(errorResponse("User with that telephoneNumber exists", userReq.lang));
+            }
+
+            const userWithSameTelephoneNumberHashed =
+                telephoneNumberHashed &&
+                (await prisma.user.findFirst({
+                    where: { telephoneNumberHashed, id: { not: id } },
+                }));
+            if (userWithSameTelephoneNumberHashed) {
+                return res
+                    .status(400)
+                    .send(
+                        errorResponse("User with that telephoneNumberHashed exists", userReq.lang)
+                    );
+            }
+
+            const user = await prisma.user.update({
+                where: { id },
+                data: {
+                    telephoneNumber,
+                    telephoneNumberHashed,
+                    emailAddress,
+                    displayName,
+                    avatarUrl,
+                },
+            });
+
+            res.send(successResponse({ user: sanitize("user", user) }));
+        } catch (e: any) {
+            le(e);
+            res.status(500).send(errorResponse(`Server error ${e}`, userReq.lang));
+        }
+    });
+
+    return router;
+};
---
+++ test/11_messenger_user.ts
@@ -0,0 +1,82 @@
+import { expect } from "chai";
+import supertest from "supertest";
+import app from "../server";
+import createFakeUser from "./fixtures/user";
+import globals from "./global";
+
+describe("User API", () => {
+    describe("/api/messenger/me GET", () => {
+        it("Should return my user", async () => {
+            const response = await supertest(app)
+                .get("/api/messenger/me")
+                .set({ accesstoken: globals.userToken });
+
+            expect(response.status).to.eqls(200);
+            expect(response.body).to.has.property("data");
+            expect(response.body.data).to.has.property("user");
+        });
+    });
+
+    describe("/api/messenger/me PUT", () => {
+        it("Requires email to be unique", async () => {
+            const fakeUser = await createFakeUser();
+            const response = await supertest(app)
+                .put("/api/messenger/me/")
+                .send({
+                    emailAddress: fakeUser.emailAddress,
+                })
+                .set({ accesstoken: globals.userToken });
+
+            expect(response.status).to.eqls(400);
+        });
+
+        it("Requires telephoneNumber to be unique", async () => {
+            const fakeUser = await createFakeUser();
+            const response = await supertest(app)
+                .put("/api/messenger/me/")
+                .send({
+                    telephoneNumber: fakeUser.telephoneNumber,
+                })
+                .set({ accesstoken: globals.userToken });
+
+            expect(response.status).to.eqls(400);
+        });
+
+        it("Requires telephoneNumberHashed to be unique", async () => {
+            const fakeUser = await createFakeUser();
+            const response = await supertest(app)
+                .put("/api/messenger/me/")
+                .send({
+                    telephoneNumberHashed: fakeUser.telephoneNumberHashed,
+                })
+                .set({ accesstoken: globals.userToken });
+
+            expect(response.status).to.eqls(400);
+        });
+
+        it("Updates user", async () => {
+            const displayName = "John";
+            const avatarUrl = "/new/avatar/url";
+            const response = await supertest(app)
+                .put("/api/messenger/me/")
+                .send({
+                    displayName,
+                    avatarUrl,
+                })
+                .set({ accesstoken: globals.userToken });
+
+            expect(response.status).to.eqls(200);
+            expect(response.body).to.has.property("data");
+            expect(response.body.data).to.has.property("user");
+            expect(response.body.data.user.displayName).to.eqls(displayName);
+            expect(response.body.data.user.avatarUrl).to.eqls(avatarUrl);
+
+            const userFromDb = await globals.prisma.user.findUnique({
+                where: { id: globals.userId },
+            });
+
+            expect(userFromDb.displayName).to.eqls(displayName);
+            expect(userFromDb.avatarUrl).to.eqls(avatarUrl);
+        });
+    });
+});
