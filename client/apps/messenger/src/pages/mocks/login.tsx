1e40fc38c640f29cfc2fe09fd822fd0b70024c07
VedranCacic <vedran.cacic@clover.studio>
2021-12-14 11:07:16 +0100

    transfered all the css into mui properties

--- client/apps/messenger/src/pages/mocks/login.tsx
+++ client/apps/messenger/src/pages/mocks/login.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import { Grid, Box, Stack, Paper, Container, TextField, Button, Link } from "@mui/material";
+import { Grid, Box, Stack, TextField, Button, Link } from "@mui/material";
 import image from "../../../../../../documents/pages/login_robot_image.svg";
 import logo from "../../../../../../documents/pages/login_logo.svg";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
-import { height, textAlign } from "@mui/system";
 
 const theme = createTheme({
     palette: {
@@ -15,25 +13,32 @@ const theme = createTheme({
 export default function () {
     return (
         <ThemeProvider theme={theme}>
-            <Grid container id="login">
-                <Grid item xs={12} md={6} lg={4} className="welcome_padding">
-                    <Stack
-                        justifyContent="start"
-                        alignItems="start"
-                        spacing={2}
-                        sx={{ height: "100%" }}
-                    >
+            <Grid container>
+                <Grid item xs={12} md={6} lg={4} pl="4em" pt="6em">
+                    <Stack justifyContent="start" alignItems="start" spacing={2}>
                         <Stack
                             justifyContent="start"
                             alignItems="start"
                             spacing={2}
                             direction="row"
                         >
-                            <img src={logo} />
-                            <label> Spika </label>
+                            <Box
+                                component="img"
+                                width="3rem"
+                                height="3rem"
+                                display="flex"
+                                flexDirection="column"
+                                justifyContent="center"
+                                src={logo}
+                            ></Box>
+                            <Box lineHeight="3rem" fontWeight="700" fontSize="1.4rem">
+                                <label> Spika </label>
+                            </Box>
                         </Stack>
-                        <Box className="welcome_title"> Welcome! </Box>
-                        <Box className="welcome_sub_title"> Sign in to start using Spika! </Box>
+                        <Box padding="1em 0 0 0" fontWeight="700" fontSize="2.4rem">
+                            Welcome!!
+                        </Box>
+                        <Box pb="3.8em"> Sign in to start using Spika! </Box>
                         <TextField
                             sx={{ width: "80%", height: 50 }}
                             id="outlined-basic"
@@ -43,17 +48,22 @@ export default function () {
                         <Button sx={{ width: "80%", height: 56 }} variant="outlined">
                             Next
                         </Button>
-                        <Box className="welcome_create">
+                        <Box pt="2em">
                             <Link href="#" underline="none">
                                 {"New here? Create account"}
                             </Link>
                         </Box>
                     </Stack>
                 </Grid>
-                <Grid item md={6} lg={8}>
-                    <Box sx={{ display: { xs: "none", md: "block" }, textAlign: "right" }}>
-                        <img className="max-height" src={image} />
-                    </Box>
+                <Grid
+                    item
+                    md={6}
+                    lg={8}
+                    display="flex"
+                    alignContent="flex-end"
+                    justifyContent="flex-end"
+                >
+                    <Box component="img" height="100vh" src={image}></Box>
                 </Grid>
             </Grid>
         </ThemeProvider>
--- client/apps/messenger/src/pages/mocks/noChat.tsx
+++ client/apps/messenger/src/pages/mocks/noChat.tsx
@@ -5,6 +5,7 @@ import { Grid, Box, Container } from "@mui/material";
 import Screenshot from "../../../../../../documents/pages/nochat.png";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { width } from "@mui/system";
 
 const theme = createTheme({
     palette: {
@@ -15,10 +16,8 @@ const theme = createTheme({
 export default function () {
     return (
         <ThemeProvider theme={theme}>
-            <Box>
-                <Grid item xs={12} md={12}>
-                    <img src={Screenshot} className="mock" />
-                </Grid>
+            <Box bgcolor="#00FF00" sx={{ width: 100, height: "100vh" }}>
+                <Grid item xs={12} md={12}></Grid>
             </Box>
         </ThemeProvider>
     );
