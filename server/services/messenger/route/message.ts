a9bc92a9227ec491e0351152c9a42e3a2fc37c53
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2022-06-01 16:52:15 +0200

    delete message API

--- server/services/messenger/route/message.ts
+++ server/services/messenger/route/message.ts
@@ -514,5 +514,100 @@ export default ({ rabbitMQChannel }: InitRouterParams): Router => {
         }
     });
 
+    router.delete("/:id", auth, async (req: Request, res: Response) => {
+        const userReq: UserRequest = req as UserRequest;
+        const userId = userReq.user.id;
+        const deviceId = userReq.device.id;
+        try {
+            const id = parseInt((req.params.id as string) || "");
+            const target = ((req.query.target as string) || "").toLowerCase();
+
+            if (!target || !["all", "user"].includes(target)) {
+                return res
+                    .status(400)
+                    .send(errorResponse("Invalid target query param", userReq.lang));
+            }
+
+            let message = await prisma.message.findFirst({
+                where: { id },
+                include: { deviceMessages: true },
+            });
+            if (!message) {
+                return res.status(404).send(errorResponse("Not found", userReq.lang));
+            }
+
+            if (target === "all" && message.fromUserId !== userId) {
+                return res
+                    .status(403)
+                    .send(errorResponse("User can't delete another users message", userReq.lang));
+            }
+
+            const roomUser = await prisma.roomUser.findFirst({ where: { roomId: message.roomId } });
+
+            if (!roomUser) {
+                return res
+                    .status(403)
+                    .send(
+                        errorResponse(
+                            "User can't delete messages from room that he is not in",
+                            userReq.lang
+                        )
+                    );
+            }
+
+            const deviceMessagesToDelete =
+                target === "all"
+                    ? message.deviceMessages
+                    : message.deviceMessages.filter((dm) => dm.userId === userId);
+
+            const newBody = { text: "Deleted message" };
+
+            for (const deviceMessage of deviceMessagesToDelete) {
+                await prisma.deviceMessage.update({
+                    where: { id: deviceMessage.id },
+                    data: { modifiedAt: new Date(), body: newBody },
+                });
+            }
+
+            if (target === "all") {
+                message = await prisma.message.update({
+                    where: { id },
+                    data: { modifiedAt: new Date(), deleted: true, type: "text" },
+                    include: { deviceMessages: true },
+                });
+            }
+
+            const sanitizedMessage = sanitize({ ...message, body: newBody }).message();
+
+            res.send(successResponse({ message: sanitizedMessage }, userReq.lang));
+
+            while (target === "all" && deviceMessagesToDelete.length) {
+                await Promise.all(
+                    deviceMessagesToDelete.splice(0, 10).map((deviceMessage) => {
+                        if (deviceMessage.deviceId === deviceId) {
+                            return;
+                        }
+
+                        rabbitMQChannel.sendToQueue(
+                            Constants.QUEUE_SSE,
+                            Buffer.from(
+                                JSON.stringify({
+                                    channelId: deviceMessage.deviceId,
+                                    data: {
+                                        type: Constants.PUSH_TYPE_DELETE_MESSAGE,
+                                        message: sanitizedMessage,
+                                    },
+                                })
+                            )
+                        );
+                    })
+                );
+            }
+        } catch (e: any) {
+            le(e);
+            res.status(500).send(errorResponse(`Server error ${e}`, userReq.lang));
+        }
+    });
+
     return router;
 };
