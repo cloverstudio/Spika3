33517dabe2de657b551c49e5473832271aa724da
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2022-10-13 10:17:21 +0200

    chore(sidebar): auto open/close on spec width

--- client/apps/messenger/src/pages/home.tsx
+++ client/apps/messenger/src/pages/home.tsx
@@ -11,7 +11,7 @@ import LeftSidebar from "../features/chat/LeftSidebar";
 import RightSidebar from "../features/chat/RightSidebar";
 import { selectLeftSidebarOpen, setLeftSidebar } from "../features/chat/slice/sidebarSlice";
 
-import { selectRightSidebarOpen } from "../features/chat/slice/rightSidebarSlice";
+import { hide, selectRightSidebarOpen, show } from "../features/chat/slice/rightSidebarSlice";
 
 import { selectUserId, fetchMe, fetchSettings } from "../../src/store/userSlice";
 
@@ -23,6 +23,26 @@ export default function Home(): React.ReactElement {
     const dispatch = useDispatch();
     const open = useSelector(selectLeftSidebarOpen);
     const rightSidebarOpen = useSelector(selectRightSidebarOpen) && !pathname.includes("/call");
+
+    useEffect(() => {
+        const resizeEventListener = (e: UIEvent) => {
+            if ((e.currentTarget as Window).innerWidth > theme.breakpoints.values.lg) {
+                if (!rightSidebarOpen) {
+                    dispatch(show());
+                }
+            } else {
+                if (rightSidebarOpen) {
+                    dispatch(hide());
+                }
+            }
+        };
+
+        window.addEventListener("resize", resizeEventListener);
+
+        return () => {
+            window.removeEventListener("resize", resizeEventListener);
+        };
+    }, [rightSidebarOpen, dispatch, theme.breakpoints.values.lg]);
 
     const loggedInUserId = useSelector(selectUserId);
 
@@ -47,9 +67,8 @@ export default function Home(): React.ReactElement {
                 sx={{
                     gridTemplateColumns: {
                         xs: "1fr",
-                        md: rightSidebarOpen
-                            ? "minmax(320px, 420px) minmax(580px, 1fr) minmax(320px, 420px)"
-                            : "minmax(320px, 420px) minmax(580px, 1fr) ",
+                        md: rightSidebarOpen ? "2fr 4fr 2fr" : "1fr 2fr",
+                        xl: rightSidebarOpen ? "1fr 3fr 1fr" : "1fr 4fr",
                     },
                 }}
             >
