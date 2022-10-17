1e27f78a724a619396589011f6568ec546799581
Stjepan Nikolić <nikolicstjepansb@gmail.com>
2022-10-17 08:32:18 +0200

    feat(notes): open note with permalink (#88)

--- client/apps/messenger/src/app.tsx
+++ client/apps/messenger/src/app.tsx
@@ -20,6 +20,7 @@ export default function App(): React.ReactElement {
 
                     <Route path="rooms" element={<HomePage />}>
                         <Route path=":id" element={<RoomPage />} />
+                        <Route path=":id/notes/:noteId" element={<RoomPage />} />
                         <Route path=":id/:messageId" element={<RoomPage />} />
                         <Route path=":id/call/lobby/:media" element={<RoomPage />} />
                         <Route path=":id/call" element={<RoomPage />} />
