5ec93d310be439161ee59c3a7ade3732595d4795
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2023-03-30 12:35:27 +0200

    rm dev login data

--- client/apps/management/src/pages/login.tsx
+++ client/apps/management/src/pages/login.tsx
@@ -10,8 +10,8 @@ import { useNavigate } from "react-router-dom";
 export default function Login() {
     const strings = useStrings();
 
-    const [username, setUsername] = useState("admin");
-    const [password, setPassword] = useState("password");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const navigate = useNavigate();
 
